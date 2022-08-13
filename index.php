82d04cb768aaefd10138fdb96492f554da7b28c3
Антон Панов <a.panov@avtoexpress.org>
2022-08-13 03:55:32 +0300

    Добалены cli утилиты для старта сервера и комманд. Добавлена комманда для вывода списка ссылок альясов.

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 define('ROOT_PATH', __DIR__);
 define('DEBUG', getenv('DEBUG'));
+define('SERVER_PORT', getenv('SERVER_PORT') ?: 8080);
+define('SERVER_HOST', getenv('SERVER_HOST') ?: "0.0.0.0");
 
 if (defined('DEBUG') && DEBUG) {
     ini_set('display_errors', 1);
--- src/Modules/Core/Commands/ListAliasesLinks.php
+++ src/Modules/Core/Commands/ListAliasesLinks.php
@@ -6,17 +6,20 @@ use Hightemp\WappTestSnotes\Modules\Core\Helpers\Utils;
 use Hightemp\WappTestSnotes\Modules\Core\Lib\BaseController;
 use Hightemp\WappTestSnotes\Modules\Core\Lib\Command;
 
-class ListAliases extends Command
+class ListAliasesLinks extends Command
 {
-    const FULL_COMMAND = "list_aliases";
+    const FULL_COMMAND = "list_aliases_links";
 
     public function fnExecute($aArgs)
     {
         $aAliases = BaseController::fnGetAllAliases();
 
+        $sHost = SERVER_HOST;
+        $iPort = SERVER_PORT;
+
         $aData = [];
         foreach ($aAliases as $sAlias => $aMethod) {
-            $aData[] = [$sAlias, ...$aMethod];
+            $aData[] = ["http://$sHost:$iPort/".$sAlias];
         }
 
         Utils::fnPrintTable($aData);
