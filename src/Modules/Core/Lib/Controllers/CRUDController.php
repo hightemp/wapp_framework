e8d67f140afbb0fb2cd3c1c10740e001b9974676
Антон Панов <a.panov@avtoexpress.org>
2022-08-14 19:16:35 +0300

    Переименован Categories в Notes. Добавил класс-генератор

---
+++ src/Modules/Core/Lib/Controllers/CRUDController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Hightemp\WappTestSnotes\Modules\Core\Lib\Controllers;
+
+use Exception;
+use Hightemp\WappTestSnotes\Modules;
+use Hightemp\WappTestSnotes\Modules\Core\Lib\Request as LibRequest;
+use Hightemp\WappTestSnotes\Modules\Core\Lib\Response;
+use Hightemp\WappTestSnotes\Modules\Core\Lib\Responses\HTML as HTMLResponse;
+use Hightemp\WappTestSnotes\Modules\Core\Lib\Responses\JSON as JSONResponse;
+use Hightemp\WappTestSnotes\Modules\Core\Lib\Responses\NotFound as NotFoundResponse;
+use Hightemp\WappTestSnotes\Modules\Core\Lib\View;
+use Request;
+use \RedBeanPHP\OODBBean;
+
+class CRUDController extends BaseController
+{
+    public static $sModelClass = '';
+
+    public function fnListJSON()
+    {
+        $sModelClass = static::$sModelClass;
+        $aList = $sModelClass::fnList($this->oRequest->aPost);
+        return $aList;
+    }
+
+    public function fnListWithPaginationJSON()
+    {
+        $sModelClass = static::$sModelClass;
+        $aList = $sModelClass::fnListWithPagination($this->oRequest->aPost);
+        return $aList;
+    }
+
+    public function fnListLastJSON()
+    {
+        $sModelClass = static::$sModelClass;
+        $aList = $sModelClass::fnListLast($this->oRequest->aPost);
+        return $aList;
+    }
+
+    public function fnDeleteJSON()
+    {
+        $sModelClass = static::$sModelClass;
+        $aList = $sModelClass::fnDelete([$this->oRequest->aPost['id']]);
+        return $aList;
+    }
+
+    public function fnDeleteListJSON()
+    {
+        $sModelClass = static::$sModelClass;
+        $aList = $sModelClass::fnDelete($this->oRequest->aPost['ids']);
+        return $aList;
+    }
+
+    public function fnCreateJSON()
+    {
+
+    }
+
+    public function fnUpdateJSON()
+    {
+
+    }
+}
