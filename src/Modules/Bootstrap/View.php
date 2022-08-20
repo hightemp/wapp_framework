132ef62d39e3074c3b349a6605d648a5e412dc68
Антон Панов <a.panov@avtoexpress.org>
2022-08-20 13:49:06 +0300

    - добавлен модуль bootstrap

---
+++ src/Modules/Bootstrap/View.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hightemp\WappTestSnotes\Modules\Bootstrap;
+
+use Hightemp\WappTestSnotes\Modules\Core\Lib\View as LibView;
+
+class View extends LibView
+{
+    public static function fnPrepareVars()
+    {
+        parent::fnPrepareVars();
+    }
+}
