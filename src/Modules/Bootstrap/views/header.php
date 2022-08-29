996c078dfa26640846b56d7810379bba3f5c4223
Антон Панов <a.panov@avtoexpress.org>
2022-08-29 09:09:32 +0300

    - добавлен css, js bootstrap

--- src/Modules/Bootstrap/views/header.php
+++ src/Modules/Bootstrap/views/header.php
@@ -1,3 +1,7 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+<?php $oTagLink("bootstrap-icons.css") ?>
+<?php $oTagLink("bootstrap.min.css") ?>
+<?php $oTagScript("jquery.min.js") ?>
+
+<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script> -->
